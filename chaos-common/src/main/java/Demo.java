34c0de9e3757fa7a4304b8a8f036cbf740fb50b4
zhiqiang.ge <zhiqiang.ge@dmall.com>
2016-08-19 15:26:29 +0800

    init

---
+++ chaos-common/src/main/java/Demo.java
@@ -0,0 +1,9 @@
+/**
+ * Description:.
+ *
+ * @author zhiqiang.ge
+ * @version V1.0
+ * @since 2016/08/19 15:22
+ */
+public class Demo {
+}
---
+++ chaos-common/src/test/java/DemoTest.java
@@ -0,0 +1,9 @@
+/**
+ * Description:.
+ *
+ * @author zhiqiang.ge
+ * @version V1.0
+ * @since 2016/08/19 15:24
+ */
+public class DemoTest {
+}

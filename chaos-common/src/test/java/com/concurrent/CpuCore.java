4906959c89936c27570cc0341accf841f05da981
zhiqiang.ge <zhiqiang.ge@dmall.com>
2016-10-10 18:44:30 +0800

    cpu 核心数

---
+++ chaos-common/src/test/java/com/concurrent/CpuCore.java
@@ -0,0 +1,15 @@
+package com.concurrent;
+
+/**
+ * Description:.
+ *
+ * @author zhiqiang.ge
+ * @version V1.0
+ * @since 2016/10/10 18:12
+ */
+public class CpuCore {
+    public static void main(String[] args) {
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+
+}
